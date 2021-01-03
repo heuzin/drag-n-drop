4ef3bf43cae4d466c81f2a09a9d8744c4d144982
heuzin <matheusqs.arq@gmail.com>
2021-01-02 19:37:49 -0500

    drag and drop API

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -20,7 +20,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
-
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -48,15 +49,13 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  // console.log('columnEl:', columnEl);
-  // console.log('column:', column);
-  // console.log('item:', item);
-  // console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
-  // Appen
+  listEl.draggable = true;
+  listEl.setAttribute('ondragstart', 'drag(event)')
+  // Append
   columnEl.appendChild(listEl)
 }
 
@@ -89,6 +88,35 @@ function updateDOM() {
   // Run getSavedColumns only once, Update Local Storage
 
 
+}
+
+// When Item Start Dragging
+function drag(e) {
+  draggedItem = e.target;
+  console.log(draggedItem)
+}
+
+// Colum allows for item to drop
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// When item enters column area
+function dragEnter(column) {
+  listColumns[column].classList.add('over')
+  currentColumn = column;
+}
+
+// Dropping item into column
+function drop(e) {
+  e.preventDefault();
+  // Remove background color/padding
+  listColumns.forEach((column) => {
+    column.classList.remove('over')
+  });
+  // Add item to column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
 }
 
 // On Load
