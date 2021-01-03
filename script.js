226277c93608423728a6d02c8be8c45fc0ee27bd
heuzin <matheusqs.arq@gmail.com>
2021-01-02 20:14:53 -0500

    updating items

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ let listArrays = [];
 
 // Drag Functionality
 let draggedItem;
+let dragging = false;
 let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
@@ -107,10 +108,14 @@ function updateDOM() {
 function updateItem(id, column) {
   const selectedArray = listArrays[column];
   const selectedColumnEl = listColumns[column].children;
-  if (!selectedColumnEl[id].textContent) {
-    delete selectedArray[id];
+  if (!dragging) {
+    if (!selectedColumnEl[id].textContent) {
+      delete selectedArray[id];
+    } else {
+      selectedArray[id] = selectedColumnEl[id].textContent;
+    }
+    updateDOM();
   }
-  updateDOM();
 }
 
 // Add to column List, reset textbox
@@ -161,6 +166,7 @@ function rebuildArrays() {
 // When Item Start Dragging
 function drag(e) {
   draggedItem = e.target;
+  dragging = true;
 }
 
 // Colum allows for item to drop
@@ -184,6 +190,8 @@ function drop(e) {
   // Add item to column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  // draggin complete
+  dragging = false;
   rebuildArrays();
 }
 
